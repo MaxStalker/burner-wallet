cda88a7a45391b26e13d24982e7589a64d895c79
Maksim Daunarovich <max.daunarovich@gmail.com>
2019-06-19 21:21:03 +0300

    Update Scan button to rumble-ui component

--- src/components/Header/Scan.js
+++ src/components/Header/Scan.js
@@ -1,46 +1,22 @@
 import React from "react";
-import { Button, Icon } from "rimble-ui";
+import { FloatingContainer, FloatingButton, CameraIcon } from "./styles";
 
 const Scan = props => {
   const { view, openScanner } = props;
-  let sendButtonOpacity = 1.0;
-  if (view === "receive" || view === "send_badge") {
-    sendButtonOpacity = 0;
-  }
+  const isHidden = view === "receive" || view === "send_badge";
+  const opacity = isHidden ? 0 : 1;
 
-  let scanButtonStyle = {
-    opacity: sendButtonOpacity,
-    position: "fixed",
-    right: 20,
-    bottom: 20,
-    zIndex: 2,
-    cursor: "pointer"
-  };
-
-  if (view === "send_to_address") {
-    scanButtonStyle.position = "absolute";
-    scanButtonStyle.right = -3;
-    scanButtonStyle.top = 217;
-    delete scanButtonStyle.bottom;
-  }
+  // TODO: check "send to address" screen on master and align positions
   return (
-    <div style={scanButtonStyle}>
-      <Button
+    <FloatingContainer opacity={opacity}>
+      <FloatingButton
         onClick={() => {
           openScanner({ view: "send_to_address" });
         }}
-        borderRadius={"50%"}
-        height={"auto"}
-        width={"auto"}
-        p={0}
-        m={0}
-        position={"absolute"}
-        bottom={3}
-        right={3}
       >
-        <Icon name="CenterFocusWeak" size={90} p={3} />
-      </Button>
-    </div>
+        <CameraIcon />
+      </FloatingButton>
+    </FloatingContainer>
   );
 };
 
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,12 +1,20 @@
 import React from "react";
 import styled from "styled-components";
-import { Box, Flex, Text, Link, Image } from "rimble-ui";
+import { Button, Box, Flex, Image, Icon, Link, Text } from "rimble-ui";
 import burnerLoading from "../../burnerloader.gif";
+
+const HeaderFlex = styled(Flex).attrs(() => ({
+  pt: 2,
+  pb: 2,
+  mb: 3,
+  alignItems: "center",
+  justifyContent: "space-between"
+}))``;
 
 export const HeaderContainer = ({ children }) => {
   return (
-    <Box pt={2} pb={2} mb={3}>
-      <Flex>{children}</Flex>
+    <Box>
+      <HeaderFlex>{children}</HeaderFlex>
     </Box>
   );
 };
@@ -19,7 +27,7 @@ export const AddressContainer = ({ children }) => {
     </Box>
   );
 };
-export const AddressContent = styled(Flex).attrs(()=>({
+export const AddressContent = styled(Flex).attrs(() => ({
   alignItems: "center"
 }))`
   cursor: pointer;
@@ -34,14 +42,14 @@ export const AccountName = ({ name }) => <Text fontSize={2}>{name}</Text>;
 
 // Blockie
 const BLOCKIE_SIZE = "48px";
-export const BlockieContainer = styled(Box).attrs(()=>({
+export const BlockieContainer = styled(Box).attrs(() => ({
   mr: 2
 }))`
   width: ${BLOCKIE_SIZE};
   height: ${BLOCKIE_SIZE};
 `;
 
-export const LoadingBlockie = styled(Image).attrs(()=>({
+export const LoadingBlockie = styled(Image).attrs(() => ({
   alt: "Loading...",
   src: burnerLoading
 }))`
@@ -50,3 +58,36 @@ export const LoadingBlockie = styled(Image).attrs(()=>({
   object-fit: cover;
   opacity: 0.25;
 `;
+
+// Balance
+export const BalanceContainer = styled(Text).attrs(() => ({
+  fontSize: 3
+}))``;
+
+// Scan Button
+export const FloatingContainer = styled(Box).attrs(({ theme }) => {
+  const { space } = theme;
+  const offset = space ? space[3] : '1em';
+  return {
+    zIndex: 5,
+    bottom: offset,
+    right: offset,
+    position: "fixed"
+  };
+})`
+  cursor: pointer;
+`;
+
+export const FloatingButton = styled(Button).attrs(() => ({
+  p: 0,
+  m: 0
+}))`
+  height: auto;
+  border-radius: 50%;
+`;
+
+export const CameraIcon = styled(Icon).attrs(() => ({
+  name: "CenterFocusWeak",
+  size: 90,
+  p: 3
+}))``;
